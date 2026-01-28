6ede26bd99168baf2b4f02c774f5fe7af9707f4a
Joel Wetzell <git@jwetzell.com>
2026-01-27 22:02:19 -0600

    Initial Commit

---
+++ OSCRing/OSCRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSCRingApp.swift
+//  OSCRing
+//
+//  Created by Joel Wetzell on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct OSCRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
